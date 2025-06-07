6cb1e21d3f35448dd903a9758d4232a2c4870e22
Lucky <111536970+Laukesh98@users.noreply.github.com>
2025-06-07 20:05:57 +0530

    Update index.jsx

--- src/Components/core/Dashboard/EditCourse/index.jsx
+++ src/Components/core/Dashboard/EditCourse/index.jsx
@@ -7,7 +7,7 @@ import {
   getFullDetailsOfCourse,
 } from "../../../../services/operations/courseDetailsAPI"
 import { setCourse, setEditCourse } from "../../../../slices/courseSlice"
-import RenderSteps from "../addCourse/RenderSteps"
+import RenderSteps from "../AddCourse/RenderSteps"
 
 export default function EditCourse() {
   const dispatch = useDispatch()
